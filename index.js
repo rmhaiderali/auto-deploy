5ba74260083746b8c9b5778a275408d6fc8926e1
Haider Ali <rmha64@gmail.com>
2023-11-26 01:19:45 +0500

    Update index.js

--- index.js
+++ index.js
@@ -35,6 +35,6 @@ app.post("/webhook", (req, res) => {
   res.status(200).send("Webhook received successfully")
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3003
 
 app.listen(PORT, () => console.log("listening on port " + PORT))
